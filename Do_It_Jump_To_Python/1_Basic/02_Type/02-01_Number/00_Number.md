e5de1f9a2e919b359af055166be45196136f6019
JeahaOh <jeaha1217@gmail.com>
2019-09-08 00:03:49 +0900

    Python Type Number

---
+++ Do_It_Jump_To_Python/1_Basic/02_Type/02-01_Number/00_Number.md
@@ -0,0 +1,6 @@
+# Number
+Python에서 사용하는 숫자의 형태는 다음과 같다
+- 정수  Integer
+- 실수  Floating-point
+- 8진수
+- 16진수
---
+++ Do_It_Jump_To_Python/1_Basic/02_Type/02-01_Number/05_Operator.py
@@ -0,0 +1,37 @@
+# coding: utf- 8
+# 사칙 연산
+print('사칙연산')
+a = 3
+b = 4
+print(a + b)
+print(b - a)
+print(a * b)
+print(a / b)
+
+
+'''
+** 연산자
+x ** y로 사용하면 x의 y제곱을 리턴.
+'''
+print('** 연산자')
+print(a ** b)
+
+'''
+% 연산자
+나누기의 나머지를 리턴.
+'''
+print('% 연산자')
+print(7 % 3)
+print(3 % 7)
+
+'''
+// 연산자
+나눗셈 후 몫을 리턴.
+'''
+print('// 연산자')
+print(7 / 4)
+print(7 // 4)
+
+print('-----------')
+print(14 // 3)
+print(14 % 3)
